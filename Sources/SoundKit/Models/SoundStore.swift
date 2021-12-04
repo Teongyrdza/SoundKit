8b3cc755c56b5b3639344a081e81283fc89f3723
Ostap <ostap.hnatenko@gmail.com>
2021-12-04 19:30:08 +0200

    Make SoundStore codable

--- Sources/SoundKit/Models/SoundStore.swift
+++ Sources/SoundKit/Models/SoundStore.swift
@@ -9,8 +9,18 @@ import Foundation
 import Combine
 
 @available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 13.0, *)
-public class SoundStore: ObservableObject {
+public class SoundStore: ObservableObject, Codable {
     @Published public var sounds: [Sound]
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(sounds)
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        sounds = try container.decode([Sound].self)
+    }
     
     public init(sounds: [Sound] = []) {
         self.sounds = sounds
